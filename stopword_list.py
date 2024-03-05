c13008c12055217674accd778b92a2483c879875
songtomtom <celvincolvumn@gmail.com>
2024-03-05 15:09:20 +0900

    불용어 리스트

--- stopword_list.py
+++ stopword_list.py
@@ -1,6 +1,20 @@
 import nltk
+
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 
+# Load stop words
 stop_words = set(stopwords.words('english'))
-print(stop_words)
+
+# Sort stop words alphabetically
+sorted_stop_words = sorted(stop_words)
+
+# Calculate the number of stop words
+num_stop_words = len(sorted_stop_words)
+
+# Print the sorted stop words
+for word in sorted_stop_words:
+    print(word)
+
+# Print the number of stop words
+print("Number of stop words: ", num_stop_words)
