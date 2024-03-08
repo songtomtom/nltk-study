add959c64ed36666cb05e0b5782757eb0bc809cd
songtomtom <celvincolvumn@gmail.com>
2024-03-08 15:28:36 +0900

    어간 확인

--- stemming_and_lemmatization.py
+++ stemming_and_lemmatization.py
@@ -1,13 +1,15 @@
 import nltk
 
-nltk.download('punkt')  # For tokenization
-nltk.download('averaged_perceptron_tagger')  # For POS tagging
-nltk.download('wordnet')  # For lemmatization
+
 
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
 
+
+nltk.download('punkt')  # For tokenization
+nltk.download('averaged_perceptron_tagger')  # For POS tagging
+nltk.download('wordnet')  # For lemmatization
 
 def get_wordnet_pos(treebank_tag):
     if treebank_tag.startswith('J'):
@@ -26,8 +28,9 @@ def get_wordnet_pos(treebank_tag):
 # text = "This was not the map we found in Billy Bones's chest, but an accurate copy, complete in all things--names and heights and soundings--with the single exception of the red crosses and the written notes."
 # text = "they are built up over time from their past uses in particular contexts by particular groups of participants in the accomplishment of particular goals that, in turn, are shaped by myriad cultural, historical and institutional forces."
 # text = "However, human reasoning is still notoriously prone to confusion and error when causal questions become sufficiently complex, such as when it comes to assessing the impact of policy interventions across society."
-text = "Children, men, women, and people walked with their feet, showing their teeth as they laughed at the geese, mice, and sheep."# Tokenization
+text = "has, have, having, had"# Tokenization
 tokenized_text = word_tokenize(text)
+print(tokenized_text)
 
 # POS tagging
 pos_tagged = nltk.pos_tag(tokenized_text)
